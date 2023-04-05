b75519fbb71a05cd79879ec200e927728f29b87f
RaptorDelta999 <drupp2001@gmail.com>
2023-04-06 01:46:36 +0200

    design update (round shapes)

--- Assets/Scripts/RoadFixer.cs
+++ Assets/Scripts/RoadFixer.cs
@@ -107,12 +107,12 @@ public class RoadFixer : MonoBehaviour
     {
         if (result[0] == CellType.Road && result[2] == CellType.Road)
         {
-            placementManager.ModifyStructureModel(temporaryPosition, roadStraight, Quaternion.Euler(0, 90, 0));
+            placementManager.ModifyStructureModel(temporaryPosition, roadStraight, Quaternion.identity);
             return true;
         }
         else if (result[1] == CellType.Road && result[3] == CellType.Road)
         {
-            placementManager.ModifyStructureModel(temporaryPosition, roadStraight, Quaternion.identity);
+            placementManager.ModifyStructureModel(temporaryPosition, roadStraight, Quaternion.Euler(0, 90, 0));
             return true;
         }
         return false;
